74f9848fb39daca61589e03e1af60d5d79f3a788
J-shw <91017208+J-shw@users.noreply.github.com>
2023-08-04 19:01:34 +0100

    Adjusted timings
    Issue with code running too quickly and causing resends to http way to fast - Fixed using sleeps and quicker shutoff

--- webhook.py
+++ webhook.py
@@ -7,9 +7,10 @@ def send(event_name, keys):
     for key in keys:
         start = time.time()
         rCode = -1
-        while rCode !=200 and time.time() < start + 5:
+        while rCode !=200 and time.time() < start + 1:
             webhook_url = "https://maker.ifttt.com/trigger/"+event_name+"/with/key/" + key
             response = requests.post(webhook_url.format(event=event_name))
             rCode = response.status_code
+            time.sleep(0.5)
     
     return rCode
