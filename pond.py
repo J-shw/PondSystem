b1fd94d5537b059e2ef9e0efece0be8f875675dd
J-shw <91017208+J-shw@users.noreply.github.com>
2024-05-09 16:02:15 +0100

    Added to update json

--- pond.py
+++ pond.py
@@ -748,6 +748,7 @@ def updateJson(data : list) -> list:
 
         config['waterLevels']['levelCheck']['refill'] = data[39]
         config['pumpControl']['enabled'] = data[40]
+        config['waterLevels']['levelCheck']['autoShutoff'] = data[41]
 
 
     
